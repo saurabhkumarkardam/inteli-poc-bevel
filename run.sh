045f17b2b354508df8cde7b9d05a4b668bf38c28
saurabhkumarkardam <saurabh.kumar.kardam@accenture.com>
2024-06-28 10:04:25 +0000

    [ci skip] Pushing deployment files for dscp api
    Signed-off-by: saurabhkumarkardam <saurabh.kumar.kardam@accenture.com>

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ echo "Adding env variables..."
 export PATH=/root/bin:$PATH
 
 #Path to k8s config file
-KUBECONFIG=/home/bevel/build/config
+export KUBECONFIG=/home/bevel/build/config
 
 echo "Validatin network yaml"
 ajv validate -s /home/bevel/platforms/network-schema.json -d /home/bevel/build/network.yaml 
